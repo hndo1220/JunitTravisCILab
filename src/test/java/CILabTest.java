30a93176138cabc314139bf54765e1de4d777ec2
Hieu Do <hieu.do08@gmail.com>
2022-06-13 06:19:28 -0700

    Wrote first five unit tests for detectCapitalUse()

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -19,14 +19,37 @@ public class CILabTest {
     }
 
     @Test
+    // Check that method returns true when All letters in this word are capitals, like "USA".
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+       myString.setString("USA");
+       assertTrue(myString.detectCapitalUse());
     }
+
+    // Check that method returns true when All letters in this word are not capitals, like "leetcode".
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("leetcode");
+        assertTrue(myString.detectCapitalUse());
     }
 
+    // Check that method returns true when Only the first letter in this word is capital, like "Google".
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("Google");
+        assertTrue(myString.detectCapitalUse());
+    }
 
+    // Check that method returns true when an all-lowercased string has an internal capitalized letter
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("leeTcode");
+        assertFalse(myString.detectCapitalUse());
+    }
+
+    // Check that method returns true when the all-capitalized string has an internal uncapitalized letter
+    @Test
+    public void detectCapitalUseTest5() {
+        myString.setString("UsA");
+        assertFalse(myString.detectCapitalUse());
+    }
 }
